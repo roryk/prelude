1c3528806f3a65b79115c2f433a8eb1c9e2fca73
roryk <rory.kirchner@gmail.com>
2013-08-29 10:21:06 -0400

    Added more personal files.

--- personal/personal-org.el
+++ personal/personal-org.el
@@ -1,3 +1,4 @@
+(require 'org)
 (setq org-todo-keywords
       '((type "TODO(t)" "WAITING(w)" "APPT(a)" "NEXT(n)" "READ(r)"
               "|"
@@ -19,5 +20,34 @@
           (tags "INBOX" ((org-agenda-files '("~/Documents/Org/notes.org"))))
           (todo "READ"))))
 )
+
+(require 'org-pomodoro)
+(setq org-pomodoro-play-sounds nil)
+(setq org-agenda-files (list "~/Documents/Org/"))
+(define-key org-agenda-mode-map "j" 'evil-next-line)
+(define-key org-agenda-mode-map "k" 'evil-previous-line)
+
+(defun esf/evil-key-bindings-for-org ()
+  ;;(message "Defining evil key bindings for org")
+  (evil-declare-key 'normal org-mode-map
+    "gk" 'outline-up-heading
+    "gj" 'outline-next-visible-heading
+    "H" 'org-beginning-of-line ; smarter behaviour on headlines etc.
+    "L" 'org-end-of-line ; smarter behaviour on headlines etc.
+    "t" 'org-todo ; mark a TODO item as DONE
+    ",c" 'org-cycle
+    (kbd "TAB") 'org-cycle
+    ",e" 'org-export-dispatch
+    ",n" 'outline-next-visible-heading
+    ",p" 'outline-previous-visible-heading
+    ",t" 'org-set-tags-command
+    ",u" 'outline-up-heading
+    "$" 'org-end-of-line ; smarter behaviour on headlines etc.
+    "^" 'org-beginning-of-line ; ditto
+    "-" 'org-ctrl-c-minus ; change bullet style
+    "<" 'org-metaleft ; out-dent
+    ">" 'org-metaright ; indent
+    ))
+(esf/evil-key-bindings-for-org)
 
 (provide 'personal-org)
--- personal/personal.el
+++ personal/personal.el
@@ -1,4 +1,12 @@
 (key-chord-mode -1)
 (setq default-tab-width 2)
-
 (setq whitespace-style (quote (face tabs trailing lines-tail)))
+
+;; this does the SUPER annoying auto-filling in of the closing paren
+(smartparens-global-mode -1)
+
+(local-set-key (kbd "RET") 'newline-and-indent)
+(load-theme 'solarized-dark t)
+
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
