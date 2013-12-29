f88bb9a5ef4787f3289e40b51d8665ef16d47957
roryk <rory.kirchner@gmail.com>
2013-12-29 01:32:52 -0500

    Add a bigger font.

--- personal/personal.el
+++ personal/personal.el
@@ -1,12 +1,17 @@
-(key-chord-mode -1)
+(key-chord-mode -1) ; this does not play well with evil-mode
+
+
 (setq tab-width 2)
 (setq whitespace-style (quote (face tabs trailing lines-tail)))
 
 ;; this does the SUPER annoying auto-filling in of the closing paren
-(smartparens-global-mode -1)
+;; (smartparens-global-mode -1)
 
 (global-set-key (kbd "RET") 'newline-and-indent)
-(load-theme 'cyberpunk t)
+(load-theme 'cyberpunk t) ; this is nice and bright
+
+;; load it like this so it is respected in new frames
+(setq default-frame-alist '((font . "Source-Code-Pro-14")))
 
 ;; any type of bell coupled with evil-mode is obnoxious
 (setq ring-bell-function 'ignore)
