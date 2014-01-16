a333739224104ab9ea32ea8b661bd95c67414496
roryk <rory.kirchner@gmail.com>
2014-01-16 14:00:31 -0500

    Add cider-namespace-refresh as a function.

--- personal/personal-clojure.el
+++ personal/personal-clojure.el
@@ -1,6 +1,16 @@
-(provide 'personal-clojure)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (setq nrepl-hide-special-buffers nil)
 (setq cider-popup-stacktraces nil)
 (setq cider-repl-popup-stacktraces t)
 (setq cider-auto-select-error-buffer nil)
+
+;(defun cider-namespace-refresh ()
+;  (interactive)
+;  (cider-interactive-eval
+;   "(require 'clojure.tools.namespace.repl)
+;    (require '[clojure.pprint :refer [pprint]])
+;    (clojure.tools.namespace.repl/refresh)"))
+
+;(define-key clojure-mode-map (kbd "M-r") 'cider-namespace-refresh)
+
+(provide 'personal-clojure)
