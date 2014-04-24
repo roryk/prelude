5f5936f633af22b01c5b5f917f33bb65c00a014c
roryk <rory.kirchner@gmail.com>
2014-04-24 12:41:28 -0400

    Remove multiple minor mode mode for Rmarkdown files.

--- personal/personal-markdown.el
+++ personal/personal-markdown.el
@@ -1,3 +1,14 @@
-
 (setq markdown-open-command "/usr/local/bin/mark")
+
+;; (require 'mmm-mode)   ;;; possibly init with (require 'mmm-auto) instead
+;; (mmm-add-classes
+;; '((rmarkdown
+;;    :submode r-mode
+;;    :face mmm-declaration-submode-face
+;;    :front "^```[{]r.*[}] *$"
+;;    :back "^``` *$")))
+;; (setq mmm-global-mode 'maybe)
+;; (mmm-add-mode-ext-class 'markdown-mode "\\.rmd\\'" 'rmarkdown)
+;; (mmm-add-mode-ext-class 'markdown-mode "\\.Rmd\\'" 'rmarkdown)
+
 (provide 'personal-markdown)
