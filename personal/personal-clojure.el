a01194c58c147e1acc375b0fe5d2f1285c3e7534
roryk <rory.kirchner@gmail.com>
2014-05-13 13:01:41 -0400

    Re-enable some of the nice cider options.

--- personal/personal-clojure.el
+++ personal/personal-clojure.el
@@ -1,16 +1,26 @@
+
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-(setq nrepl-hide-special-buffers nil)
-(setq cider-popup-stacktraces nil)
-(setq cider-repl-popup-stacktraces t)
-(setq cider-auto-select-error-buffer nil)
+(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
+(add-hook 'cider-repl-mode-hook 'subword-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
-;(defun cider-namespace-refresh ()
-;  (interactive)
-;  (cider-interactive-eval
-;   "(require 'clojure.tools.namespace.repl)
-;    (require '[clojure.pprint :refer [pprint]])
-;    (clojure.tools.namespace.repl/refresh)"))
+(setq nrepl-hide-special-buffers t)
+(setq cider-stacktrace-default-filters '(java clj))
+(setq nrepl-buffer-name-show-port t)
+(setq cider-repl-print-length 25)
+(setq cider-prompt-save-file-on-load nil)
 
-;(define-key clojure-mode-map (kbd "M-r") 'cider-namespace-refresh)
 
-(provide 'personal-clojure)
+;; (setq cider-popup-stacktraces nil)
+;; (setq cider-repl-popup-stacktraces t)
+
+;; (defun cider-namespace-refresh ()
+;;  (interactive)
+;;  (cider-interactive-eval
+;;   "(require 'clojure.tools.namespace.repl)
+;;    (require '[clojure.pprint :refer [pprint]])
+;;    (clojure.tools.namespace.repl/refresh)"))
+
+;; (define-key clojure-mode-map (kbd "M-r") 'cider-namespace-refresh)
+
+;; (provide 'personal-clojure)
