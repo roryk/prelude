5348c5b2ea864695eeec54e66f03ac586b3d04fc
roryk <rory.kirchner@gmail.com>
2014-06-02 12:18:27 -0400

    Roll back annoying crossing lines behavior.

--- personal/personal-evil.el
+++ personal/personal-evil.el
@@ -2,8 +2,8 @@
 
 (prelude-require-packages '(evil-paredit))
 
-(setq evil-cross-lines t)
-(setq evil-move-cursor-back nil)
+;(setq evil-cross-lines t)
+;(setq evil-move-cursor-back t)
 
 (define-key evil-normal-state-map ";" 'smex)
 (define-key evil-visual-state-map ";" 'smex)
