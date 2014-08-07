9957f49b4a5c1cf60b9a01cb02c5246d10dcb1b1
roryk <rory.kirchner@gmail.com>
2014-08-06 20:00:43 -0400

    Remove delay to zero since it is set in prelude-evil now.

--- personal/personal-evil.el
+++ personal/personal-evil.el
@@ -2,9 +2,6 @@
 
 (prelude-require-packages '(evil-paredit))
 
-;; prevent esc-key from translating to meta-key in terminal mode
-(setq evil-esc-delay 0)
-
 (define-key evil-normal-state-map ";" 'smex)
 (define-key evil-visual-state-map ";" 'smex)
 
