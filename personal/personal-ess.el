35b9f76c2d09d277a835296fd9d1fd997580fc55
roryk <rory.kirchner@gmail.com>
2014-12-02 12:11:19 -0500

    Disable company-mode, this was causing some slowdowns in R.

--- personal/personal-ess.el
+++ personal/personal-ess.el
@@ -1,17 +1,28 @@
-(prelude-require-packages '(ess))
+(prelude-require-packages '(ess polymode))
 
 (require 'ess-site)
 ;; turn off this ultra annoying replace the underscore mode
 (ess-toggle-underscore nil)
 
+
 ;; auto-complete can be ultra slow in R buffers, so disable it
-(setq ess-use-auto-complete nil)
+;;(setq ess-use-auto-complete nil)
 ;; company-mode is also superbadslow
 
-(add-hook 'ess-mode-hook
-          (lambda ()
-            (company-mode 0)))
+;;add-hook 'ess-mode-hook
+  ;;        (lambda ()
+   ;;         (company-mode 0)))
 
-(setq ess-use-eldoc-nill)
+;;(setq ess-use-eldoc-nil)
+
+;; (defun rmd-mode ()
+;;   "ESS Markdown mode for rmd files"
+;;   (interactive)
+;;   (setq load-path
+;;         (append (list "path/to/polymode/" "path/to/polymode/modes/")
+;;                 load-path))
+;;   (require 'poly-R)
+;;   (require 'poly-markdown)
+;;   (poly-markdown+r-mode))
 
 (provide 'personal-ess)
